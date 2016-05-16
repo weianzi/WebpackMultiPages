e3a813081334a16c2d75a5a02b74805860591159
ancool <ancool@foxmail.com>
2016-05-16 14:04:58 +0800

    del

--- README.md
+++
@@ -1,7 +0,0 @@
-利用webpack 构建多页面应用
-
-npm i
-
-npm start
-
-npm run build
--- src/js/list.js
+++
@@ -1,6 +0,0 @@
-require('../css/common.css');
-require('../css/b.css');
-var Comp1 = require('./components/comp1.js');
-
-console.log('我是列表页面, 我开始调用了组件Comp1了');
-Comp1();
